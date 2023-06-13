423a99cd6dab43b20bf0535dd43a5bf002af91b6
Anaëlle JODRY <anaelle.jodry@corp.sophia.mines-paristech.fr>
2023-06-13 10:53:51 +0200

    New readme + use of highs

--- single-area-multi-period.py
+++ single-area-multi-period.py
@@ -6,6 +6,7 @@ import pyomo.environ as pyo
 from pyomo.opt import SolverFactory
 import time 
 import os 
+import highspy
 
 from Functions.f_multiResourceModelsAna import systemModel,loadScenario
 from Functions.f_optimization import getVariables_panda, getConstraintsDual_panda
@@ -14,39 +15,56 @@ from scenarios_ref_PACA import scenarioPACA
 # from scenarios import scenario
 
 outputPath='Data/output/'
-solver= 'cbc'
+# solver= 'appsi_highs'
+
+
+solverpath_folder='C:\\Users\\anaelle.jodry\\Documents\\highs.mswin64.20230531'
+sys.path.append(solverpath_folder)
 
 print('Building model...')
 model = systemModel(scenarioPACA,isAbstract=False)
-
+model.write("test_model.mps", io_options = {"symbolic_solver_labels":True})
 start_clock = time.time()
 print('Calculating...')
-opt = SolverFactory(solver)
-results = opt.solve(model)
+h = highspy.Highs()
+filename = 'test_model.mps'
+h.readModel(filename)
+h.run()
+print('Model ', filename, ' has status ', h.getModelStatus())
+solution = h.getSolution()
+info = h.getInfo()
+basis = h.getBasis()
+# # opt = SolverFactory(solver)
+# results = opt.solve(model)
 end_clock = time.time()
 print('Computational time: {:.0f} s'.format(end_clock - start_clock)) 
 
-res = {
-    'variables': getVariables_panda(model), 
-}
+print('Optimal objective = ', info.objective_function_value)
+num_var = h.getNumCol()
+for icol in range(num_var):
+    print(icol, solution.col_value[icol], h.basisStatusToString(basis.col_status[icol]))
 
-try: 
-    res['constraints'] = getConstraintsDual_panda(model)
-except KeyError: # This exception will be raised for a MILP case
-    pass 
+# res = {
+#     'variables': getVariables_panda(model), 
+# }
 
-outputFolder = 'out'
+# try: 
+#     res['constraints'] = getConstraintsDual_panda(model)
+# except KeyError: # This exception will be raised for a MILP case
+#     pass 
 
-try: 
-    os.mkdir(outputFolder)
-except: 
-    pass
+# outputFolder = 'out_test_highs'
 
-for v in res['variables'].keys():
-    print(v)
+# try: 
+#     os.mkdir(outputFolder)
+# except: 
+#     pass
 
-for k, v in res['variables'].items():
-    print ('Writing ' + k + '...') 
-    v.to_csv(outputFolder + '/' + k + '.csv',index=True)
+# for v in res['variables'].keys():
+#     print(v)
 
-print(res['variables']['capacity_Pvar'])
+# for k, v in res['variables'].items():
+#     print ('Writing ' + k + '...') 
+#     v.to_csv(outputFolder + '/' + k + '.csv',index=True)
+
+# print(res['variables']['capacity_Pvar'])
